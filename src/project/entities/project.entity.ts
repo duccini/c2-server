ec72c2644c90168282c391d1595a475fd76eb70a
tulio <tuliovictorp@hotmail.com>
2024-07-17 21:53:35 -0300

    feat: entity project

--- src/project/entities/project.entity.ts
+++ src/project/entities/project.entity.ts
@@ -1,1 +1,32 @@
-export class Project {}
+import { Exclude } from 'class-transformer';
+import { UUID } from 'crypto';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { ProjectStatus } from '../enums/project-status.enum';
+
+@Entity('project')
+export class Project {
+  @PrimaryGeneratedColumn('uuid')
+  id?: UUID;
+
+  @Column('text')
+  title: string;
+
+  @Column('text')
+  description: string;
+
+  @Column({
+    type: 'enum',
+    enum: ProjectStatus,
+  })
+  @Column('date')
+  created_at: Date;
+
+  @Column('text')
+  lead: string;
+
+  @Column('text')
+  teams: string;
+
+  @Column('date')
+  finished_at: Date;
+}
