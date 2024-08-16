ad13677614de9abba821bab3b7e4db0eb22261b0
Jefferson Nunes <nunesjefferson425@gmail.com>
2024-08-16 19:52:50 -0300

    alter connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,35 @@
 import { Module } from '@nestjs/common';
 import { ProjectModule } from './project/project.module';
 import { ConfigModule } from '@nestjs/config';
+
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { TeamsModule } from './teams/teams.module';
+import { Project } from './project/entities/project.entity';
+import { User } from './users/entities/user.entity';
 import { AuthModule } from './auth/auth.module';
-import { typeOrmConfig } from './configs/typeorm.config';
+
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot(typeOrmConfig),
     ProjectModule,
+    // TypeOrmConfigModule,
+    TypeOrmModule.forRoot({
+      type: process.env.DB_TYPE as any,
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      autoLoadEntities: true,
+      entities: [Project, User],
+      synchronize: true,
+    }),
     UsersModule,
     TeamsModule,
     AuthModule,
   ],
-  exports: [],
   controllers: [],
   providers: [],
 })
