3aa43c0d98da1066194c06a8775a962343a4385f
duccini <guilhermeduccini@gmail.com>
2024-08-19 11:11:48 -0300

    fix typeorm connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,34 +1,25 @@
 import { Module } from '@nestjs/common';
 import { ProjectModule } from './project/project.module';
 import { ConfigModule } from '@nestjs/config';
-import { TypeOrmConfigModule } from './configs/typeOrmConfig.module';
+import { typeOrmConfig } from './configs/typeorm.config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { TeamsModule } from './teams/teams.module';
-import { Project } from './project/entities/project.entity';
-import { User } from './users/entities/user.entity';
 import { AuthModule } from './auth/auth.module';
-import { Skill } from './skills/entities/skill.entity';
 import { SkillsModule } from './skills/skills.module';
 import { RolesModule } from './roles/roles.module';
+
+import { Skill } from './skills/entities/skill.entity';
+import { User } from './users/entities/user.entity';
+import { Project } from './project/entities/project.entity';
 import { Role } from './roles/entities/role.entity';
+import { TypeOrmConfigModule } from './configs/typeOrmConfig.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ProjectModule,
-    // TypeOrmConfigModule,
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as any,
-      host: process.env.PG_HOST,
-      port: parseInt(process.env.PG_PORT),
-      username: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DB,
-      autoLoadEntities: true,
-      entities: [Project, User, Skill, Role],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
     UsersModule,
     TeamsModule,
     AuthModule,
