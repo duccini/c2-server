b8e4b7a66e73155a1e7644c24d4fc510b3d5a05a
Jefferson Nunes <nunesjefferson425@gmail.com>
2024-09-04 21:50:22 -0300

    alteracoes para implementação do frontend

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,78 +4,76 @@ import { AuthPayloadDto } from './dto/auth.dto';
 import { UsersService } from 'src/users/users.service';
 import { ApiTags } from '@nestjs/swagger';
 import { RequestPayloadDto } from './dto/request.dto';
-import { resetPasswordPayloadDto } from './dto/resetPasswrod.dto';
+import { ResetPasswordPayloadDto } from './dto/resetPassword.dto';
 
 @ApiTags('users')
 @Controller('auth')
 export class AuthController {
-  
-  constructor(private authService: AuthService, private usersService: UsersService
 
+  constructor(
+    private authService: AuthService, 
+    private usersService: UsersService
   ) {}
 
-  //ROTA DE LOGIN
+  // Rota de login
   @Post('login')
   async login(@Body() { email, password }: AuthPayloadDto) {
-
     try {
-
-        const result = await this.authService.validateUser(email,password);
-
-        return result; // objeto contendo o token e informações do usuário
-
+      const result = await this.authService.validateUser(email, password);
+      return result;  // retorna token e informações do usuário
     } catch (error) {
-
-       throw new HttpException(error.message, HttpStatus.UNAUTHORIZED); //  erros
+      throw new HttpException(error.message, HttpStatus.UNAUTHORIZED);
     }
   }
 
-
-  //ROTA PARA ENVIAR EMAIL DE RESET DE SENHA
+  // Rota para enviar e-mail de reset de senha
   @Post('request-password-reset')
+  async requestPasswordReset(@Body() { email }: RequestPayloadDto) {
+    try {
+      const user = await this.usersService.findByEmail(email);
 
-  async requestPasswordReset(@Body() {email}: RequestPayloadDto){
+      if (!user) {
+        throw new NotFoundException('Usuário não encontrado');
+      }
 
-   try {
-    const user = await this.usersService.findByEmail(email);
+      const token = await this.authService.genarationPasswordResetToken(user);
+      await this.authService.sendPasswordResetEmail(user, token);
 
-    if(!user){
-      throw new NotFoundException('Usuário não encontrado');
+      return { message: "E-mail de redefinição de senha enviado" };
+    } catch (error) {
+      console.error("Erro ao enviar e-mail de redefinição de senha:", error);
+      throw new HttpException('Erro ao processar a solicitação de redefinição de senha.', HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
 
-    const token = await this.authService.genarationPasswordResetToken(user);
-    await this.authService.sendPasswordResetEmail(user,token)
-
-    return {message: "E-mail de redefinição de senha enviado"}
-    
-   } catch (error) {
-    console.log(error)    
-   }
-  }  
-
-
-  //ROTA DE RESETAR SENHA 
-  @Post('reset-password')
-  async resetPassword(
-     @Body() {token,newPassword}: resetPasswordPayloadDto
-) {
+  @Post('validate-reset-token')
+async validateResetToken(@Body() { token }: ResetPasswordPayloadDto) {
   const user = await this.usersService.findByResetToken(token);
+  
+  if (!user || user.resetTokenExpires < new Date()) {
+    throw new BadRequestException('Token inválido ou expirado');
+  }
+  
+  // O token está correto, envia um OK ao frontend
+  return { message: 'Token válido', success: true };
+}
+
+@Post('reset-password')
+async resetPassword(@Body() { token, password }: ResetPasswordPayloadDto) {
+  const user = await this.usersService.findByResetToken(token);
+
   if (!user || user.resetTokenExpires < new Date()) {
     throw new BadRequestException('Token inválido ou expirado');
   }
 
-  
-  const passwordHash = await this.usersService.hashPassword(newPassword);
+  const passwordHash = await this.usersService.hashPassword(password);
   user.password = passwordHash;
 
-  //RESENTANDO DADOS DO REQUEST DE NOVA SENHA
+  // Limpa o token e salva a nova senha
   user.resetToken = null;
   user.resetTokenExpires = null;
- 
- 
-  //SALVANDO NOVA SENHA 
-  await this.usersService.save(user);
 
+  await this.usersService.save(user);
   return { message: 'Senha redefinida com sucesso' };
 }
 }
